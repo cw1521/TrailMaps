3120f74cbc6d1a6b9ed52ca1058c444b55a926f0
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-11-04 07:55:09 -0500

    removed some dependencies

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = require("./routes/routes");
 const path = require("path");
-const cors = require("cors");
 
 
 const PORT = process.env.PORT || 5000;
 
 const WEBSITE_PATH = path.join(__dirname, "website");
-var corsOptions = {
-    origin: "*"
-}
+
 
 const app = express();
 
-app.use(cors(corsOptions))
 app.use(express.static(WEBSITE_PATH));
 
 
