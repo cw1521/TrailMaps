70b53216ddc8695b62b6679fd7f28e066748169f
Christian Williams <cw1521@jagmail.southalabama.edu>
2023-11-04 14:26:59 -0500

    moved models to script.js

--- website/js/models/KeyModel.js
+++
@@ -1,9 +0,0 @@
-class KeyModel {
-    constructor(id, name, api_key) {
-        this.id = id;
-        this.name = name;
-        this.api_key = api_key;
-    }
-}
-
-module.exports = KeyModel;
--- website/script.js
+++ website/script.js
@@ -127,7 +127,12 @@ class TrailModel {
     }
 
 
-};
+}
 
-
-
+class KeyModel {
+    constructor(id, name, api_key) {
+        this.id = id;
+        this.name = name;
+        this.api_key = api_key;
+    }
+}
